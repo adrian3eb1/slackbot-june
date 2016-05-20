e998d8900b83cb52786d8111fea7f7305ae5d5e1
May Kosaka <may.kosaka@gmail.com>
2016-05-19 21:15:05 -0700

    edited may.js

--- scripts/may.js
+++ scripts/may.js
@@ -1,5 +1,19 @@
 module.exports = function(robot) {
-	robot.hear(/Hello!!/, function(res) {
-	   return res.send("Hi there!");
-	 });
-}
+	var lulz;
+  robot.hear(/bug/i, function(res) {
+    return res.send("junebug or VW bug?");
+  });
+  robot.respond(/open the (.*) doors/i, function(res) {
+    var doorType;
+    doorType = res.match[1];
+    if (doorType === "pod bay") {
+      return res.reply("I'm afraid I can't let you do that.");
+    } else {
+      return res.reply("Opening " + doorType + " doors");
+    }
+  });
+  robot.hear(/welcome june/i, function(res) {
+    return res.emote("maybees don't fly in june");
+  });
+  return lulz = ['lol', 'rofl', 'lmao'];
+};
